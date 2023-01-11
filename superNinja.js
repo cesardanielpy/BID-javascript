fe2fcde3bd79b00afe1018b120a69250f2bef9a0
Cesar Ayala <cesardaniel.608@gmail.com>
2023-01-11 02:44:17 -0300

    Correccion de algunos errores

--- superNinja.js
+++ superNinja.js
@@ -1,7 +1,7 @@
 class ninja {
     constructor(nombre, salud) {
         this.nombre = nombre;
-        this.salud = salud, + "%";
+        this.salud = 3;
         this.velocidad = 3;
         this.fuerza = 3;
     }
@@ -23,22 +23,16 @@ class ninja {
 class Sensei extends ninja {
     constructor(nombre, salud, velocidad, fuerza, sabiduria){
         super(nombre, salud, velocidad, fuerza, sabiduria);
-        salud = salud + this.speakWisdom;
         this.velocidad = 10;
         this.fuerza= 10;
         this.sabiduria = 10;
+        this.salud = 200;
     }
-    showStats(){
-        console.log(`Nombre: ${this.nombre} Fuerza: ${this.fuerza} Velocidad: ${this.velocidad} Salud: ${this.salud}`)
-    } 
     speakWisdom(){
         super.drinkSake();
-    }
-    saludPlus(){
-        console.log(`Salud ${this.salud}`);
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
     }
 }
-
 let superSensei = new Sensei("Oden",200);
 
 console.log(superSensei);
